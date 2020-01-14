7992568378c56102cb7a28555ad1a143372a560f
nhols <neil_holloway@hotmail.co.uk>
2020-01-14 22:55:51 +0000

    made a start on some scraping data of each listing

--- id_grabber.py
+++ id_grabber.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-import numpy as  np
-
+import numpy as np
+import re
 listing_ids = []
 search_zones = {'ZONE1':'920364979', 'ZONE2':'920365348', 'ZONE3':'920365945', 'ZONE4':'920366158', 'ZONE5':'920366257', 'ZONE6':'920366374'}
 
@@ -27,3 +27,18 @@ for z in search_zones:
         for listing in parsed_rsp.find_all("li", class_="listing-result"):
             listing_ids.append(listing['data-listing-id'])
 
+
+for id in listing_ids:
+
+    rsp = requests.get(f'https://www.spareroom.co.uk/flatshare/flatshare_detail.pl?flatshare_id={id}')
+    parsed_rsp = BeautifulSoup(rsp.content, 'html.parser')
+
+    lat = re.findall(r'\"\d+\.\d+\"', parsed_rsp.find_all('script')[2].text)
+    lon = re.findall(r'\"-\d+\.\d+\"', parsed_rsp.find_all('script')[2].text)
+
+    for room in parsed_rsp.find_all('li', {'class' : 'room-list__room'}):
+        print(room.find('strong').text)
+        print(room.find('small').text)
+
+    feature_keys = parsed_rsp.find_all('dt', {'class': 'feature-list__key'})
+    feature_values = parsed_rsp.find_all('dt', {'class': 'feature-list__value'})
